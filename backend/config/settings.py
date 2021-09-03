d1b7ba4789a859a08f040529414fe15d8052a3b4
deekshith-hari <deekshith@musubi-management.com>
2021-09-03 23:16:09 +0530

    cloudinary settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -155,9 +155,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 cloudinary.config(
-    cloud_name="techis",
-    api_key="886187759951178",
-    api_secret="WGcWrWfkPlxdOCdlFaLTdjSNtfo",
+    cloud_name="dop22f4ta",
+    api_key="265567725696858",
+    api_secret="Kjos3iv_duQ4POVDTQtVcgwV5L4",
     secure=True
 )
 
