d5fe5c16d19411cdb28be4dae0e3fa5ac00d7736
deekshith-hari <deekshith@musubi-management.com>
2021-09-17 03:04:15 +0530

    added signin page

--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, Switch } from "react-router";
 import Home from "./containers/Home";
+import Signin from "./containers/Signin";
 
 const Router = () => {
   return (
     <>
       <Switch>
         <Route exact path={"/"} component={Home} />
+        <Route exact path={"/signin"} component={Signin} />
       </Switch>
     </>
   );
